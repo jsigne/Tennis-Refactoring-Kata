fc6186c85f5a9c3defe93c50e1ab252a2c68dc21
JSigne <signe.josie@gmail.com>
2023-04-02 12:03:33 +0200

    josie refacto

---
+++ java/src/main/java/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    private final String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
