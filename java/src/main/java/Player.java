0a72d82244883cbcebea49ecd68a2a9bcb32d4d9
JSigne <signe.josie@gmail.com>
2023-04-05 12:31:42 +0200

    maxime refacto score

--- java/src/main/java/Player.java
+++ java/src/main/java/Player.java
@@ -7,7 +7,20 @@ public class Player {
         this.score = 0;
     }
 
-    public int getScore() {
+    public int score() {
         return score;
+    }
+
+    public void addOneToScore() {
+        score += 1;
+    }
+
+    public StringBuilder scoreAsStringBuilder() {
+        return switch (score) {
+            case 0 -> new StringBuilder("Love-All");
+            case 1 -> new StringBuilder("Fifteen-All");
+            case 2 -> new StringBuilder("Thirty-All");
+            default -> new StringBuilder("Deuce");
+        };
     }
 }
--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,9 +1,5 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-
     private final Player player1;
     private final Player player2;
 
@@ -14,26 +10,21 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1"))
-            m_score1 += 1;
+            player1.addOneToScore();
         else
-            m_score2 += 1;
+            player2.addOneToScore();
     }
 
     public String getScore() {
         StringBuilder score = new StringBuilder();
         int tempScore = 0;
-        if (m_score1 == m_score2)
+        if (player1.score() == player2.score())
         {
-            score = switch (m_score1) {
-                case 0 -> new StringBuilder("Love-All");
-                case 1 -> new StringBuilder("Fifteen-All");
-                case 2 -> new StringBuilder("Thirty-All");
-                default -> new StringBuilder("Deuce");
-            };
+            return player1.scoreAsStringBuilder().toString();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        if (player1.score()>=4 || player2.score()>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = player1.score()-player2.score();
             if (minusResult==1) score = new StringBuilder("Advantage player1");
             else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
             else if (minusResult>=2) score = new StringBuilder("Win for player1");
@@ -43,8 +34,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score.append("-"); tempScore = m_score2;}
+                if (i==1) tempScore = player1.score();
+                else { score.append("-"); tempScore = player2.score();}
                 switch (tempScore) {
                     case 0 -> score.append("Love");
                     case 1 -> score.append("Fifteen");
